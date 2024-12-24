467d0e3191c847a1760232a4365292fda8d7b420
Alex Brown <github@alexbrown.info>
2024-12-24 22:33:07 +0000

    feat: Add "Save All and Continue" (has to be manually enabled)

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # lazygit-confirm.nvim
 
-A tiny plugin to check if there are any unsaved buffers before running [Snacks](https://github.com/folke/snacks.nvim).lazygit.
+A hacked-together plugin to check if there are any unsaved buffers before running [Snacks](https://github.com/folke/snacks.nvim).lazygit.
 
 ![](img/confirm.png)
 
 ## Why?
 
-I use Snacks.lazygit regularly to launch lazygit in a NeoVim window by using `<leader>gg`.
+As a LazyVim user, I use Snacks.lazygit regularly to launch lazygit in a NeoVim window by using `<leader>gg`.
 However, I kept forgetting to save all my work before running lazygit.
 I would commit my changes, realise that I hadn't saved everything, and then have to amend my last commit.
 
@@ -17,6 +17,9 @@ For Lazy.nvim:
 return {
   {
     "thelastpsion/lazygit-confirm.nvim",
+    opts = {
+      -- options
+    }
   },
 }
 ```
@@ -28,4 +31,21 @@ keymap.set("n", "<leader>gg", function()
 end, { noremap = true })
 ```
 
+## Options
+
+```lua
+{
+  show_saveall = true            -- Shows the "Save All and Continue" option. You will also be asked to confirm.
+  show_saveall_noconfirm = true  -- Same as above, but without the extra confirmation dialog. Overrides show_saveall.
+}
+```
+
+## Notes
+
+By default, lazygit-confirm doesn't ask you if you want to save all buffers before continuing.
+This was a deliberate choice -- I wanted to force myself to check the modified buffers, just in case the change to the buffer was an accident.
+However, a few people have suggested that I should add it, so I've included it as an option.
+
+Also, lazygit-confirm doesn't check for any dependencies.
+If Snacks or fzf-lua are installed, lazygit-confirm will fail ungracefully.
 
--- lua/lazygit-confirm.lua
+++ lua/lazygit-confirm.lua
@@ -1,12 +1,19 @@
 local M = {}
 
--- M.setup = function()
--- 	vim.print("lazygit-confirm loaded")
--- end
+M.setup = function(opts)
+  vim.print("lazygit-confirm loaded")
+  M.show_saveall = opts.show_saveall or false
+  M.show_saveall_noconfirm = opts.show_saveall_noconfirm or false
+end
 
 M.confirm = function()
   local unsaved_table = {}
   local choice = 1
+  local saveall = ""
+  local run_lazygit = false
+  if M.show_saveall == true or M.show_saveall_noconfirm == true then
+    saveall = "\nSave &All and Continue"
+  end
   for _, buf_id in ipairs(vim.api.nvim_list_bufs()) do
     if vim.api.nvim_get_option_value("modified", { buf = buf_id }) then
       table.insert(unsaved_table, string.format("%3d", buf_id) .. ": " .. vim.api.nvim_buf_get_name(buf_id))
@@ -15,14 +22,28 @@ M.confirm = function()
   if #unsaved_table ~= 0 then
     choice = vim.fn.confirm(
       "There are unsaved buffers:\n\n" .. table.concat(unsaved_table, "\n") .. "\n\nDo you still want to run lazygit?",
-      "&Yes\n&No\nShow &Buffers",
+      "&Yes\n&No\nShow &Buffers" .. saveall,
       2
     )
   end
   if choice == 1 then
-    Snacks.lazygit()
+    run_lazygit = true
   elseif choice == 3 then
     vim.cmd([[FzfLua buffers sort_mru=true sort_lastused=true query='+']]) -- HACK: What happens when files contain `+`?
+  elseif choice == 4 then
+    local confirm = 2
+    if M.show_saveall_noconfirm == false then
+      confirm = vim.fn.confirm("Are you sure you want to save all buffers?", "&Yes\n&No", 2)
+    end
+    if confirm == 1 or M.show_saveall_noconfirm == true then
+      vim.cmd("wall")
+      vim.print("All buffers have been saved.")
+      run_lazygit = true
+    end
+  end
+
+  if run_lazygit == true then
+    Snacks.lazygit()
   end
 end
 
